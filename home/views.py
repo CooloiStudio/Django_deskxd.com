d0b40da4047d609715e1a7f6cacf06aad8999c87
biansetuliao <TMWQ@tumiuweingdeAir.deskxd.org>
2015-08-18 18:21:26 +0800

    update css and html and js

--- home/views.py
+++ home/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class IndexView(generic.View):
+    templates_file = 'Index.html'
+
+    def get(self, request):
+        context = {
+
+        }
+        return render(request,
+                      self.templates_file,
+                      context)
