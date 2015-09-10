6274e09a22d6eb7a87774f0d9aa8d4aa670c6000
biansetuliao <TMWQ@tumiuweingdeAir.deskxd.org>
2015-09-10 13:51:14 +0800

    update wsgi.py

--- deskxd_com/wsgi.py
+++ deskxd_com/wsgi.py
@@ -7,14 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os, sys
+import os
 
 from django.core.wsgi import get_wsgi_application
-
-path = '/home/active/Django_deskxd.com'
-if path not in sys.path:
-        print "====== Not in! ======"
-        sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deskxd_com.settings")
 
