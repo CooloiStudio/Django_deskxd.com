71226cb34c8fc393a4c5f1a4ed79f5e21fa5e3c6
biansetuliao <biansetuliao@gmail.com>
2015-10-14 11:58:44 +0800

    update

--- deskxd_com/urls.py
+++ deskxd_com/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
     url(r'^game/', include('game.urls', namespace="game")),
     url(r'^information/', include('information.urls', namespace="information")),
     url(r'^member/', include('member.urls', namespace="member")),
+    url(r'^thanks/', include('thanks.urls', namespace="thanks")),
 )
