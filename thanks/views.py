52ecab2e548a79c8cf009a8b1b22d2e3362be58b
biansetuliao <biansetuliao@gmail.com>
2015-10-16 10:37:42 +0800

    create thanks models

--- thanks/views.py
+++ thanks/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.views import generic
 from django.http import HttpResponse, HttpResponseRedirect
+from thanks.models import *
+import random
 
 class IndexView(generic.View):
     templates_file = 'thanksIndex.html'
 
     def get(self, request):
 
+        thankss = list(Thanks.objects.all())
+        if thankss:
+            random.shuffle(thankss)
+        else:
+            thankss = []
+
         context = {
+            'thankss': thankss,
         }
         return render(request,
                       self.templates_file,
