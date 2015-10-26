b514c99c3dcea3c2c6f98fadb3afdab7f16666db
biansetuliao <biansetuliao@gmail.com>
2015-10-26 14:45:13 +0800

    update migrate

--- game/migrations/0001_initial.py
+++ game/migrations/0001_initial.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
@@ -10,6 +10,15 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.CreateModel(
+            name='GameImages',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('code', models.IntegerField(default=0, unique=True)),
+                ('img', models.CharField(max_length=300)),
+                ('url', models.CharField(max_length=300)),
+            ],
+        ),
         migrations.CreateModel(
             name='Games',
             fields=[
