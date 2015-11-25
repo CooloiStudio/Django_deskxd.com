c89116201088f01bc731a2e87deb9b7ef758aa2c
biansetuliao <biansetuliao@gmail.com>
2015-11-25 17:39:28 +0800

    create game admin.py

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from game.models import *
 
-# Register your models here.
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', )
+
+class GameImagesAdmin(admin.ModelAdmin):
+    list_display = ('code', 'img', )
+
+admin.site.register(Games, GameAdmin)
+admin.site.register(GameImages, GameImagesAdmin)
