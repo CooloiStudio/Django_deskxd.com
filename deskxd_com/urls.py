7eab3bf7e58023de7bb043bcb80243f294a4fc69
biansetuliao <biansetuliao@gmail.com>
2015-12-10 17:51:49 +0800

    add static url

--- deskxd_com/urls.py
+++ deskxd_com/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.conf.urls import patterns, include, url
+from django.contrib.auth import settings
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
@@ -10,4 +11,5 @@ urlpatterns = patterns('',
     url(r'^thanks/', include('thanks.urls', namespace="thanks")),
     url(r'^agreement/', include('agreement.urls', namespace="agreement")),
     url(r'^ueditor/',include('DjangoUeditor.urls')),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
