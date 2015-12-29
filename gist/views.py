61c5ca02983e513c2bf6810c592d6f96f2066cc0
biansetuliao <biansetuliao@gmail.com>
2015-12-29 21:12:12 +0800

    update

--- gist/views.py
+++ gist/views.py
@@ -18,8 +18,7 @@ class IndexViews(generic.View):
                 if posts:
                     post_list = []
                     for post in posts:
-                        body = markdown(post.body)
-                        a = {'id': post.id, 'title': post.title, 'body': body, 'date': post.date}
+                        a = {'id': post.id, 'title': post.title, 'body': post.body, 'date': post.date}
                         post_list.append(a)
                     b = {'categorys': p.name, 'post': post_list}
                 else:
@@ -48,8 +47,7 @@ class InfoViews(generic.View):
             if posts:
                 post_list = []
                 for post in posts:
-                    body = markdown(post.body)
-                    a = {'category': post.category.name, 'title': post.title, 'body': body, 'date': post.date}
+                    a = {'category': post.category.name, 'title': post.title, 'body': post.body, 'date': post.date}
                     post_list.append(a)
             else:
                 post_list = []
